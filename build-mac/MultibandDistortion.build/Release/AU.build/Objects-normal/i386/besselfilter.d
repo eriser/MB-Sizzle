c07653abc3217fdf51bd4cd2d0bf92203a39e468
Michael Donovan <mdonovan159@gmail.com>
2016-03-10 14:38:29 -0600

    2x oversampling

---
+++ build-mac/MultibandDistortion.build/Release/AU.build/Objects-normal/i386/besselfilter.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/michael/PluginDev/wdl-ol-michaeldonovan/IPlugExamples/EECS352-Multiband-Distortion-Plugin/besselfilter.cpp \
+  /Users/michael/PluginDev/wdl-ol-michaeldonovan/IPlugExamples/EECS352-Multiband-Distortion-Plugin/besselfilter.h \
+  /Users/michael/PluginDev/wdl-ol-michaeldonovan/IPlugExamples/EECS352-Multiband-Distortion-Plugin/../../WDL/wdltypes.h \
+  /Users/michael/PluginDev/wdl-ol-michaeldonovan/IPlugExamples/EECS352-Multiband-Distortion-Plugin/denormal.h
