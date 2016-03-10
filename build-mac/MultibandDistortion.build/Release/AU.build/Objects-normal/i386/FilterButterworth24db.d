c4833a834818dfc7a6f58bcff036f96b8bedef78
Michael Donovan <mdonovan159@gmail.com>
2016-03-10 01:12:50 -0600

    Filterbank tweaks
    still getting odd distortion

---
+++ build-mac/MultibandDistortion.build/Release/AU.build/Objects-normal/i386/FilterButterworth24db.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/michael/PluginDev/wdl-ol-michaeldonovan/IPlugExamples/EECS352-Multiband-Distortion-Plugin/FilterButterworth24db.cpp \
+  /Users/michael/PluginDev/wdl-ol-michaeldonovan/IPlugExamples/EECS352-Multiband-Distortion-Plugin/FilterButterworth24db.h
