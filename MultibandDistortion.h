43553b98dc2857a5abac084fa9f55812bdae54a9
Michael Donovan <mdonovan159@gmail.com>
2016-03-10 00:46:03 -0600

    Oversampling

--- MultibandDistortion.h
+++ MultibandDistortion.h
@@ -93,6 +93,10 @@ private:
   
   const int fftSize=4096;
   const int channelCount = 2;
+  const int mOversampling;
+  
+  WDL_BesselFilterCoeffs mAntiAlias;
+  WDL_BesselFilterStage mUpsample, mDownsample;
   
   int mDistType;
   int mPolynomials;
